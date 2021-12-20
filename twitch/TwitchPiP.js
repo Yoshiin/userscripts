1b8c929a4a0883a81bfb8725b4c82bae9f24a999
Léo Mora <l.mora@outlook.fr>
2021-12-20 14:58:39 +0100

    Add license field

--- twitch/TwitchPiP.js
+++ twitch/TwitchPiP.js
@@ -4,6 +4,7 @@
 // @version      1.0
 // @description  Add a PiP button on Twitch player
 // @author       Yoshin
+// @license      MIT
 // @match        https://www.twitch.tv/*
 // @icon         https://www.google.com/s2/favicons?domain=twitch.tv
 // @grant        none
