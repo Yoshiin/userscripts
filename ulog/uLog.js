87b0502a8040ea2f3678b678a91ce5be25cd087d
Léo Mora <l.mora@outlook.fr>
2023-07-25 10:50:39 +0200

    Update withStyle method

--- ulog/uLog.js
+++ ulog/uLog.js
@@ -24,6 +24,14 @@ class Logger {
         };
     }
 
+    disableLogger() {
+        this.enabled = false;
+    }
+
+    enableLogger() {
+        this.enabled = true;
+    }
+
     configColors(configObj) {
         let newColorsConfig = this.colors;
         Object.keys(configObj).forEach(key => {
@@ -34,44 +42,48 @@ class Logger {
         this.colors = newColorsConfig;
     }
 
-    baseLog(logType, logName, logStr, logSub, ...style) {
+    baseLog(logType, logName, logStr, logSub, style) {
         if (this.enabled) {
-            if (!logSub) {
-                window.console[`${logType}`](`%c${this.appName}[%c${logName}%c]:%c ${logStr}`,
+            if (logSub === null) {
+                window.console[`${logType}`](`%c${this.appName} [%c${logName}%c]:%c ${logStr}`,
                                              `color: ${this.colors.main}`,
                                              `color: ${this.colors[`${logName}`]}`,
                                              `color: ${this.colors.main}`,
                                              `color: ${this.colors.str}`,
-                                             ...style
+                                             ...(style ?? [])
                                             );
             } else {
-                window.console[`${logType}`](`%c${this.appName}(%c${logSub}%c)[%c${logName}%c]:%c ${logStr}`,
+                window.console[`${logType}`](`%c${this.appName}(%c${logSub}%c) [%c${logName}%c]:%c ${logStr}`,
                                              `color: ${this.colors.main}`,
                                              `color: ${this.colors.sub}`,
                                              `color: ${this.colors.main}`,
                                              `color: ${this.colors[`${logName}`]}`,
                                              `color: ${this.colors.main}`,
                                              `color: ${this.colors.str}`,
-                                             ...style
+                                             ...(style ?? [])
                                             );
             }
         }
     }
 
-    info(str, subScriptName = undefined) {
+    info(str, subScriptName = null) {
         this.baseLog('info', 'info', str, subScriptName)
     }
 
-    error(str, subScriptName = undefined) {
+    error(str, subScriptName = null) {
         this.baseLog('error', 'error', str, subScriptName)
     }
 
-    run(str, subScriptName = undefined) {
+    run(str, subScriptName = null) {
         this.baseLog('info', 'run', str, subScriptName)
     }
 
-    withStyle(str, subScriptName = undefined, ...style) {
-        this.baseLog('info', 'info', str, subScriptName, ...style)
+    withStyle(str, subScriptName = null, style) {
+        if (Array.isArray(subScriptName)) {
+            style = subScriptName;
+            subScriptName = null;
+        }
+        this.baseLog('info', 'info', str, subScriptName, style)
     }
 
     raw(type, obj) {
@@ -80,25 +92,4 @@ class Logger {
             window.console[`${type}`](obj);
         }
     }
-
-    /*
-    static elmOp(type, name, desc) {
-        if (this.enabled) {
-            switch (type) {
-                case 'new':
-                    window.console.info(`%cETT(%cELM%c) [%cinfo%c]:%c New card found for %c${name}%c: title set to "%c${desc}%c"`,
-                                 'color: #BF94FF', 'color: #9AA0A6', 'color: #BF94FF', 'color: #F2AB26', 'color: #BF94FF', 'color: #FFFFFF', 'font-weight: bolder', 'font-weight: normal', 'font-style: italic', 'font-style: normal');
-                    break;
-                case 'update':
-                    window.console.info(`%cETT(%cELM%c) [%cinfo%c]:%c Update card for %c${name}%c: title updated to "%c${desc}%c"`,
-                                 'color: #BF94FF', 'color: #9AA0A6', 'color: #BF94FF', 'color: #F2AB26', 'color: #BF94FF', 'color: #FFFFFF', 'font-weight: bolder', 'font-weight: normal', 'font-style: italic', 'font-style: normal;');
-                    break;
-                case 'noupdate':
-                default:
-                    window.console.info(`%cETT(%cELM%c) [%cinfo%c]:%c Update card for %c${name}%c: noting to do`,
-                                 'color: #BF94FF', 'color: #9AA0A6', 'color: #BF94FF', 'color: #F2AB26', 'color: #BF94FF', 'color: #FFFFFF', 'font-weight: bolder', 'font-weight: normal');
-            }
-        }
-    }
-    */
 }
